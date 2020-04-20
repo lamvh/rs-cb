000330518381e41c811b2276d2f526950a0d2324
lam <vhlam1997@gmail.com>
2020-04-21 01:29:42 +0700

    update page title, info

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/lamvh/rs-cb.git"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
